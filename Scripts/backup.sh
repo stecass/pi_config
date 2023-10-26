0090bdd45b9a42497061a9df29391f52357242d4
Stephen <albytrott@gmail.com>
2023-10-26 19:05:20 +0100

    added -s switch to backup scriot

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -6,13 +6,25 @@ hs=`hostname`
 
 now=$(date +"%d_%m_%y")
 
+BACKUPFILE=/mnt/backup/Pi_Backups/$hs-${now}.img
+DONEFILE="$BACKUPFILE".done
+
 printf "Current date and time %s\n" "$now"
 
 echo Backup running "$now">> /var/log/backups
 
-libsmount="/mount/nfs/backup/";
+libsmount="/mnt/backup/Pi_Backups/";
 
-dd bs=4M of=/mnt/backup/Pi_Backups/$hs-${now}.img if=/dev/mmcblk0 >>  /var/log/backup
+dd bs=4M of=$BACKUPFILE if=/dev/mmcblk0 >>  /var/log/backups
+
+echo "shrinking the image with pishrink" >> /var/log/backups
+
+if test -f /usr/local/bin/pishrink.sh; then
+        echo "shrinking the image with pishrink" >> /var/log/backups
+    	pishrink.sh -s "$BACKUPFILE"
+fi
+
 now="$(date)"
-
 echo Backup finished "$now">> /var/log/backups
+
+touch "$DONEFILE"
